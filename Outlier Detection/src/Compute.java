f5c0ef9c2ba6f3a3dc34630713563d5cd3bd44e3
Alvaro Perez Ramon <alvaro.perez.ramon@gmail.com>
2014-10-02 15:02:02 +0200

    trying solution with ReadWriteReentrantLock

--- Outlier Detection/src/Compute.java
+++ Outlier Detection/src/Compute.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,12 +16,13 @@ public class Compute {
 	public static boolean keys = false;
 	public static int count=0;
 	public static int numThreads=0;
+	final static ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
 
-	private static class ThreadRead extends Thread {
+	private static class ThreadWrite extends Thread {
 		public FileManager reader;
 		public List<File> files;
 
-		public ThreadRead(List<File> files, FileManager reader){
+		public ThreadWrite(List<File> files, FileManager reader){
 			super();
 			this.reader=reader;
 			this.files=files;
@@ -28,7 +31,7 @@ public class Compute {
 		public void run() {
 			while(controlRead) {
 				try {
-					read();
+					write();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -36,27 +39,27 @@ public class Compute {
 			}
 		}
 
-		public void read() throws InterruptedException{
-			synchronized(info){
-				while(count==numThreads){
-					int i=0;
-					for (File file: files){
-						System.out.println("vuelta: "+i);
-						i++;
-						reader.parse(file, info);
-						keys=true;
-						System.out.println(info.getInfo().get("de Brandenburger_Tor"));
-						//System.out.println(info.getInfo());
-						while(info.getSizeDeque()>10){
-							count=0;
-							info.wait();
-							notifyAll();
-						}
+		public void write() throws InterruptedException{
+			int i=0;
+			for (File file: files){
+				if(!rwl.isWriteLocked()){
+					System.out.println("Cojo el cerrojo que escribe");
+					rwl.writeLock().lock();
+				}
+				System.out.println("vuelta: "+i);
+				i++;
+				reader.parse(file, info);
+				keys=true;
+				System.out.println(info.getInfo().get("de Brandenburger_Tor"));
+				while(info.getSizeDeque()>10){
+//					System.out.println("Suelto el cerrojo que escribe");
+					if(rwl.isWriteLocked()){
+						System.out.println("Suelto el cerrojo que escribe");
+						rwl.writeLock().unlock();
 					}
 				}
-				controlRead=false;
 			}
-
+			controlRead=false;
 		}
 	}
 
@@ -87,53 +90,52 @@ public class Compute {
 		}
 
 		public void compute() throws InterruptedException{
-			synchronized(info.getInfo().get(key)){
-				if(count!=numThreads){
-					info.wait();
-					DescriptiveStats ds = new DescriptiveStats(info.getInfo().get(key));
-					density.densityX(info.getInfo().get(key),ds.getMean(), info.getInfo().get(key).getLast());
-					//				if (id==3){
-					//					System.out.println(density.getDensities());
-					//				}
-					if(density.getDensities().size()>10){
-						density.getDensities().removeFirst();
-					}
-					if(controlRead){
-						info.getInfo().get(key).removeFirst();
-						count++;
-						info.notify();
-						info.getInfo().get(key).wait();
-					}
-					if(info.isEmptyDeque(key)){
-						controlCompute=false;
-					}
+			//			System.out.println("Aqui estamos "+ id);
+			//			DescriptiveStats ds = new DescriptiveStats(info.getInfo().get(key));
+			//			density.densityX(info.getInfo().get(key),ds.getMean(), info.getInfo().get(key).getLast());
+			//			if(density.getDensities().size()>10){
+			//				density.getDensities().removeFirst();
+			//			}
+			rwl.readLock().lock();
+			System.out.println("Cojo el cerrojo que lee");
+			info.getInfo().get(key).removeFirst();
+			if(controlRead ){
+				if (key.equals("de Brandenburger_Tor")){
+					System.out.println("saco el valor: "+info.getInfo().get(key).removeFirst());
 				}
+				System.out.println("Suelto el cerrojo que lee");
+				rwl.readLock().unlock();
+			}
+			if(info.isEmptyDeque(key)){
+				controlCompute=false;
 			}
 		}
+
 	}
 
 	public static void main (String[] args) throws InterruptedException{
-		double time_start = System.currentTimeMillis();
 		FileManager fm = new FileManager();
 		String rootDirectory = "/Users/alvaro/Documents/TUGraz/Master Thesis/TrainingSet/";
 		List<File> files = fm.listDirectories(rootDirectory);
-		count=numThreads;
-		ThreadRead threadRead = new ThreadRead(files, fm);
-		threadRead.start();
+		ThreadWrite threadWrite = new ThreadWrite(files, fm);
+		threadWrite.start();
 		while (!keys){
 			System.out.print("");
 		}
 		int i=0;
-		count=info.getInfo().keySet().size();
-		System.out.println("La cuenta por favor " + count);
-		for(String key: info.getInfo().keySet()){
-			ThreadCompute tc= new ThreadCompute(key, i, new Density());
-			i++;
-			tc.start();
-		}
-		threadRead.join();
-		double time_end = System.currentTimeMillis();
-		System.out.println("the task has taken "+ ( time_end - time_start ) +" milliseconds");
+		ThreadCompute tc = new ThreadCompute ("de Brandenburger_Tor", 1, new Density());
+		tc.start();
+		//		List<Thread> threads = new ArrayList<Thread>();
+		//		for(String key: info.getInfo().keySet()){
+		//			ThreadCompute tc= new ThreadCompute(key, i, new Density());
+		//			threads.add(tc);
+		//			i++;
+		//			tc.start();
+		//		}
+		threadWrite.join();
+		//		for(Thread t:threads){
+		//			t.join();
+		//		}
 	}
 } 
 
